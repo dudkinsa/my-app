c07a8d8e21bce59b4126f959c4ff9d4f3b1606c6
Sergey Dudkin <dsergik@mail.ru>
2022-01-30 22:33:25 +0300

    добавление модулей css

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Header.module.css';
 
 const Header = () =>{
     return(
-        <header className='header'>
+        <header className={classes.header}>
             <img
                 src='https://images.fastcompany.net/image/upload/w_596,c_limit,q_auto:best,f_auto/fc/3034007-inline-i-applelogo.jpg'/>
         </header>
--- src/componets/Profile.jsx
+++ src/componets/Profile.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import classes from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={classes.content}>
             <div>
-                <img className='img-background'
+                <img
                      src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'/>
             </div>
             <di>
@@ -16,11 +17,11 @@ const Profile = () => {
             <div>
                 New post
             </div>
-            <div>
-                <div>
+            <div className={classes.posts}>
+                <div className={classes.item}>
                     post1
                 </div>
-                <div>
+                <div className={classes.item}>
                     post2
                 </div>
             </div>
