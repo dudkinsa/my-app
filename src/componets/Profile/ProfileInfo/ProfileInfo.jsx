7dea57a5b02918f44fa32daf2d00e2c6cbd60f8a
Dudkin Sergey <dsergik@mail.ru>
2022-02-03 11:52:31 +0300

    lesson 24 crated folder ProfilInfo and crated two files ProfileInfo.jsx and ProfileInfo.module.css

---
+++ src/componets/Profile/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import classes from './ProfileInfo.module.css';
+import MyPosts from "../../Profile/MyPosts/MyPosts";
+
+
+const ProfileInfo = () => {
+    return (
+        <div>
+
+            <div>
+                <img
+                    src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'
+                    alt='lake'/>
+            </div>
+            <div>
+                ava + description
+            </div>
+        </div>
+    );
+}
+export default ProfileInfo;
